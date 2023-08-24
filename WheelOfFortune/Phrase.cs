85351e38ca834d17135d96140384ed8bbff13983
ZclipseCode <bmichaelbusiness@gmail.com>
2023-08-24 01:26:36 -0500

    Add project files.

---
+++ WheelOfFortune/Phrase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Phrase
+{
+    public string spelling;
+    public Category category;
+    public string visibleCategory;
+
+    public Phrase(string spelling, Category category, string visibleCategory)
+    {
+        this.spelling = spelling;
+        this.category = category;
+        this.visibleCategory = visibleCategory;
+    }
+}
---
+++ WheelOfFortune/WheelOfFortune.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class WheelOfFortune
+{
+    public static void Main()
+    {
+        List<Phrase> phrases = new List<Phrase>();
+        Phrase p1 = new Phrase("THIS IS A TEST", Category.WhatAreYouDoing, "WHAT ARE YOU DOING?");
+        phrases.Add(p1);
+
+        Random random = new Random();
+        Phrase currentPhrase = phrases[random.Next(phrases.Count)];
+
+        string guess = "";
+        for (int i = 0; i < currentPhrase.spelling.Length; i++)
+        {
+            if (currentPhrase.spelling[i] == ' ')
+            {
+                guess += "  ";
+            }
+            else
+            {
+                guess += "_ ";
+            }
+        }
+
+        Console.WriteLine(guess);
+    }
+}
