3f8f32f501e75bc00f2ad91bb8e56d4b37aef60f
ZclipseCode <bmichaelbusiness@gmail.com>
2023-08-24 01:50:16 -0500

    added descriptions

--- WheelOfFortune/Category.cs
+++ WheelOfFortune/Category.cs
@@ -3,41 +3,76 @@ using System.ComponentModel;
 
 public enum Category
 {
+    [Description("AROUND THE HOUSE")]
     AroundTheHouse,
+    [Description("BEFORE & AFTER")]
     BeforeAndAfter,
+    [Description("BEST SELLER(S)")]
     BestSellers,
+    [Description("CHARACTER(S)")]
     Characters,
+    [Description("CLASSIC TV")]
     ClassicTV,
+    [Description("COLLEGE LIFE")]
     CollegeLife,
+    [Description("EVENT(S)")]
     Events,
+    [Description("FAMILY")]
     Family,
+    [Description("FUN & GAMES")]
     FunAndGames,
+    [Description("FOOD & DRINK")]
     FoodAndDrink,
+    [Description("HEADLINE")]
     Headline,
+    [Description("HUSBAND & WIFE")]
     HusbandAndWife,
+    [Description("IN THE KITCHEN")]
     InTheKitchen,
+    [Description("LANDMARK(S)")]
     Landmarks,
+    [Description("LIVING THING(S)")]
     LivingThings,
+    [Description("MOVIE QUOTE")]
     MovieQuote,
+    [Description("TV QUOTE")]
     TVQuote,
+    [Description("OCCUPATIONS")]
     Occupations,
+    [Description("ON THE MAP")]
     OnTheMap,
-    People,
+    [Description("PERSON / PEOPLE")]
+    PersonPeople,
+    [Description("PHRASE(S)")]
     Phrases,
+    [Description("PLACE(s)")]
     Places,
+    [Description("PROPER NAME(S)")]
     ProperNames,
+    [Description("QUOTATION")]
     Quotation,
+    [Description("RYHME TIME")]
     RhymeTime,
+    [Description("SAME LETTER")]
     SameLetter,
+    [Description("SAME NAME")]
     SameName,
+    [Description("SHOW BIZ")]
     ShowBiz,
-    SongAndArtist,
+    [Description("SONG / ARTIST")]
+    SongArtist,
+    [Description("SONG LYRICS")]
     SongLyrics,
+    [Description("STAR & ROLE")]
     StarAndRole,
+    [Description("THING(S)")]
     Things,
+    [Description("TITLE(S)")]
     Titles,
-    TitleAndAuthor,
+    [Description("TITLE / AUTHOR")]
+    TitleAuthor,
     [Description("WHAT ARE YOU DOING?")]
     WhatAreYouDoing,
+    [Description("WHAT ARE YOU WEARING?")]
     WhatAreYouWearing
 }
